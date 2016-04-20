62bfdd8a97dbc9ce43f7471c429c6f4bb415882e
Necdet Can Ateşman <can@strg.at>
2016-04-20 11:35:01 +0200

    Release: 0.2.19

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.cli',
-    version='0.2.18',
+    version='0.2.19',
     description='Command line interface to The SCORE Framework',
     long_description=README,
     author='strg.at',
